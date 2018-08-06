b64701b097f6d6c894ba0c0b570304d4b98f6e9c
sonir <isana@sonir.jp>
2018-08-06 14:06:57 +0900

    Ver1.0 :: Legend in 2018a

---
+++ init.sh
@@ -0,0 +1,7 @@
+
+echo "# mcell_m4l" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Sonilab/mcell_m4l.git
+git push -u origin master
